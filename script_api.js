7417e37289dde7c76db6d6d078fa47d053fce1d2
Helen Cristina <95002354+helencrs@users.noreply.github.com>
2022-06-13 22:43:26 -0300

    Delete script_api.js

--- script_api.js
+++
@@ -1,8 +0,0 @@
-function api(){
-fetch('https://api.disneyapi.dev/characters')
-    .then(response => response.json())
-    .then(data =>  imagem(data));
-}
-    function imagem(data){
-        console.log(data)
-}
